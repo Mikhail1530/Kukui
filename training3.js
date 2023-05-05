d71ae00df02985507b26d4ffd5b40f6112741475
Mikhail1530 <michael.nfnr@gmail.com>
2023-05-05 15:03:21 +0400

    Solution is done

---
+++ training3.js
@@ -0,0 +1,11 @@
+// TASK
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+// SOLUTION
+function reverseWords(str) {
+    return str.split(' ').map(a=>String(a).split('').reverse().join('')).join(' ')
+  }
