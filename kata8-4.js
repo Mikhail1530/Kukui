e4544e6af9e237a0b1bfb3041a7758c1cf3f88fc
Mikhail1530 <michael.nfnr@gmail.com>
2023-05-06 17:24:20 +0400

    d0ne!

---
+++ kata8-4.js
@@ -0,0 +1,13 @@
+// TASK
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+SOLUTION 
+
+function digitize(n) {
+    return [...String(n)].reverse().map(Number);
+   }
