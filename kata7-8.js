1c10094bca9233dfa1333c28feec4c011276f885
Mikhail1530 <michael.nfnr@gmail.com>
2023-05-09 13:52:53 +0400

    complete!

---
+++ kata7-8.js
@@ -0,0 +1,17 @@
+// Task
+// Create a function that takes in a list of button inputs and returns the final state.
+
+// Examples
+// likeOrDislike([Dislike]) => Dislike
+// likeOrDislike([Like,Like]) => Nothing
+// likeOrDislike([Dislike,Like]) => Like
+// likeOrDislike([Like,Dislike,Dislike]) => Nothing
+// Notes
+// If no button is currently active, return Nothing.
+// If the list is empty, return Nothing.
+
+SOLUTION
+
+function likeOrDislike(buttons) {
+    return buttons.reduce((a, b) => b === a ? Nothing : b, Nothing)
+}
