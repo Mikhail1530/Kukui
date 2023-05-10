1660b608aad78fbf0edbbc5096e2be4f0173cd26
Mikhail1530 <michael.nfnr@gmail.com>
2023-05-10 21:40:02 +0400

    complete

---
+++ kata8-12.js
@@ -0,0 +1,23 @@
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1
+
+SOLUTION
+function gimme(triplet) {
+    if (triplet.length === 3) {
+        let t = String(triplet).split(',').sort((a, b) => a - b)
+        if (t[1] == triplet[0]) { return triplet.indexOf(triplet[0]) }
+        else if (t[1] == triplet[1]) { return triplet.indexOf(triplet[1]) }
+        else if (t[1] == triplet[2]) { return triplet.indexOf(triplet[2]) }
+    }
+}
